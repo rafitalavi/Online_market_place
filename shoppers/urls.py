c3605f62bd0fdabe62271167dd929881722f01c7
rafitalavi <rafitalavi12345@gmail.com>
2023-12-19 22:32:32 +0600

    9th commit

--- shoppers/urls.py
+++ shoppers/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
     path('',index , name ='index'),
+    path('dashboard/', include('dashboard.urls')),
    
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
